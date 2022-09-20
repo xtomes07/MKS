7af9b5fe453de97c92efb6965a456b937f421eea
Jirka <juratomesek@seznam.cz>
2022-09-20 13:40:31 +0200

    led_morseovka

--- CV_1/Src/main.c
+++ CV_1/Src/main.c
@@ -17,6 +17,7 @@
  */
 
 #include <stdint.h>
+#include <stm32f0xx.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -24,6 +25,26 @@
 
 int main(void)
 {
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0; // output log0 for pin5
+
+	uint8_t pole[32]  ={1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0};
+
     /* Loop forever */
-	for(;;);
+	for(;;)
+		{
+			for(uint8_t i=0; i<32;i++)
+				{
+					if (pole[i]==1)
+					{
+						GPIOA->BSRR = (1<<5); // set
+					}
+					else
+					{
+						GPIOA->BRR = (1<<5); // reset
+					}
+
+					for (volatile uint32_t i = 0; i < 500000; i++){}
+				}
+		}
 }
