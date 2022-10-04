53eb7d9ff66e4e6c04bb705bded7b1e416dfa02e
Jirka <juratomesek@seznam.cz>
2022-10-04 12:21:29 +0200

    cv_2.2

--- CV_2/Src/main.c
+++ CV_2/Src/main.c
@@ -19,6 +19,15 @@
 #include <stdint.h>
 #include <stm32f0xx.h>
 
+void EXTI0_1_IRQHandler(void)
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1<<0);//toggle
+	}
+ }
+
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
@@ -31,6 +40,15 @@ int main(void)
 	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPRD1_0; // S1 = PC1, pullup
+
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // clock enable SYSCFG
+
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+
+
 
 	/* Loop forever */
 	for(;;)
