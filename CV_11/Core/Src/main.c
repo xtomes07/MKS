3220ad361bc4afc712d0774ba91bb90b7088c15a
Jirka <juratomesek@seznam.cz>
2022-12-06 14:10:23 +0100

    CV_11_malovaniKomplet

--- CV_11/Core/Src/main.c
+++ CV_11/Core/Src/main.c
@@ -38,6 +38,7 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define HID_MOUSE_REPORT_DESC_SIZE 101
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -109,6 +110,64 @@ void halfCircle(uint32_t radius){
 		sy = sy - dy;
 	}
 }
+
+__ALIGN_BEGIN static uint8_t HID_MOUSE_ReportDesc[HID_MOUSE_REPORT_DESC_SIZE] __ALIGN_END =
+{
+		/* 47 */
+		0x05, 0x01, // USAGE_PAGE (Generic Desktop)
+		0x09, 0x06, // USAGE (Keyboard)
+		0xa1, 0x01, // COLLECTION (Application)
+		0x85, 0x01, // REPORT_ID (1)
+		0x05, 0x07, // USAGE_PAGE (Keyboard)
+		0x19, 0xe0, // USAGE_MINIMUM (Keyboard LeftControl)
+		0x29, 0xe7, // USAGE_MAXIMUM (Keyboard Right GUI)
+		0x15, 0x00, // LOGICAL_MINIMUM (0)
+		0x25, 0x01, // LOGICAL_MAXIMUM (1)
+		0x75, 0x01, // REPORT_SIZE (1)
+		0x95, 0x08, // REPORT_COUNT (8)
+		0x81, 0x02, // INPUT (Data,Var,Abs)
+		0x95, 0x01, // REPORT_COUNT (1)
+		0x75, 0x08, // REPORT_SIZE (8)
+		0x81, 0x03, // INPUT (Cnst,Var,Abs)
+		0x95, 0x06, // REPORT_COUNT (6)
+		0x75, 0x08, // REPORT_SIZE (8)
+		0x15, 0x00, // LOGICAL_MINIMUM (0)
+		0x25, 0x65, // LOGICAL_MAXIMUM (101)
+		0x05, 0x07, // USAGE_PAGE (Keyboard)
+		0x19, 0x00, // USAGE_MINIMUM (Reserved (no event indicated))
+		0x29, 0x65, // USAGE_MAXIMUM (Keyboard Application)
+		0x81, 0x00, // INPUT (Data,Ary,Abs)
+		0xc0, // END_COLLECTION
+		/* 54 */
+		0x05, 0x01, // USAGE_PAGE (Generic Desktop)
+		0x09, 0x02, // USAGE (Mouse)
+		0xa1, 0x01, // COLLECTION (Application)
+		0x09, 0x01, // USAGE (Pointer)
+		0xa1, 0x00, // COLLECTION (Physical)
+		0x85, 0x02, // REPORT_ID (2)
+		0x05, 0x09, // USAGE_PAGE (Button)
+		0x19, 0x01, // USAGE_MINIMUM (Button 1)
+		0x29, 0x03, // USAGE_MAXIMUM (Button 3)
+		0x15, 0x00, // LOGICAL_MINIMUM (0)
+		0x25, 0x01, // LOGICAL_MAXIMUM (1)
+		0x95, 0x03, // REPORT_COUNT (3)
+		0x75, 0x01, // REPORT_SIZE (1)
+		0x81, 0x02, // INPUT (Data,Var,Abs)
+		0x95, 0x01, // REPORT_COUNT (1)
+		0x75, 0x05, // REPORT_SIZE (5)
+		0x81, 0x03, // INPUT (Cnst,Var,Abs)
+		0x05, 0x01, // USAGE_PAGE (Generic Desktop)
+		0x09, 0x30, // USAGE (X)
+		0x09, 0x31, // USAGE (Y)
+		0x09, 0x38, // USAGE (Wheel)
+		0x15, 0x81, // LOGICAL_MINIMUM (-127)
+		0x25, 0x7f, // LOGICAL_MAXIMUM (127)
+		0x75, 0x08, // REPORT_SIZE (8)
+		0x95, 0x03, // REPORT_COUNT (3)
+		0x81, 0x06, // INPUT (Data,Var,Rel)
+		0xc0, // END_COLLECTION
+		0xc0, // END_COLLECTION
+};
 
 /* USER CODE END PFP */
 
